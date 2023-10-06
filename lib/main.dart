99b6b7b478892561bd0ff7084cba11575c577233
mubarakbacancy <mubarak.ansari@bacancy.com>
2023-10-06 15:35:38 +0530

    Add Tab Bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/navigationdrawer/MyNavigationDrawer.dart';
 
 import 'bottomsheet/MyBottomSheet.dart';
+import 'mytabbar/MyTabBar.dart';
 
 void main() {
-  runApp(const MyBottomSheet());
+  runApp(const MyTabBar());
 }
+
---
+++ lib/mytabbar/MyTabBar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyTabBar extends StatelessWidget {
+  const MyTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Tab Bar"),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Home",
+                  icon: Icon(Icons.home),
+                ),
+                Tab(
+                  text: "Search",
+                  icon: Icon(Icons.search),
+                ),
+                Tab(
+                  text: "Setting",
+                  icon: Icon(Icons.settings),
+                ),
+              ],
+              indicatorColor: Colors.red,
+              indicator: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.red),
+              indicatorWeight: 5,
+              indicatorPadding: const EdgeInsets.all(5),
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(
+                Icons.home,
+                size: 100,
+              ),
+              Icon(
+                Icons.search,
+                size: 100,
+              ),
+              Icon(
+                Icons.settings,
+                size: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
